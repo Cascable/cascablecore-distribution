bb847698581f253f97a5e214f06f7c63132ad4ec
Daniel Kennett <daniel@cascable.se>
2026-01-15 20:37:51 +0100

    Release CascableCore 17.0.1b1

--- CascableCore.xcframework/ios-arm64_x86_64-simulator/CascableCore.framework/Headers/CBLCoreVersions.h
+++ CascableCore.xcframework/ios-arm64_x86_64-simulator/CascableCore.framework/Headers/CBLCoreVersions.h
@@ -1,4 +1,4 @@
 /// Returns a detailed version string for the SDK.
-static NSString * const CBLCoreVerboseVersion NS_SWIFT_NAME(CascableCoreVerboseVersion) = @"17.0.0-g2c4ecb53";
+static NSString * const CBLCoreVerboseVersion NS_SWIFT_NAME(CascableCoreVerboseVersion) = @"17.0.0-3-g05739b71";
 /// Returns the build number for the SDK.
-static NSString * const CBLCoreBundleVersion NS_SWIFT_NAME(CascableCoreBundleVersion) = @"2132";
+static NSString * const CBLCoreBundleVersion NS_SWIFT_NAME(CascableCoreBundleVersion) = @"2135";
